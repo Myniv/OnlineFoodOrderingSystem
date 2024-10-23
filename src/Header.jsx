8cb83fe3e134d774dfef5466a851fe365e9cf497
Mulyana N <mulyanan.ur26@gmail.com>
2024-10-23 14:49:44 +0700

    Add Header

---
+++ src/Header.jsx
@@ -0,0 +1,62 @@
+import mynivLogo from "./img/MynivLogo1.png";
+import cart from "./img/cart.png";
+const Header = () => {
+  return (
+    <div className="container">
+      <header className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+        <div className="col-md-3 mb-2 mb-md-0">
+          <a
+            href="/"
+            className="d-inline-flex link-body-emphasis text-decoration-none"
+          >
+            <img
+              className="img-fluid"
+              src={mynivLogo}
+              alt="Myniv"
+              style={{ width: "50px", height: "50px" }}
+            />{" "}
+          </a>
+        </div>
+
+        <ul className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+          <li>
+            <a href="#" className="nav-link px-2 link-secondary">
+              Menu
+            </a>
+          </li>
+          <li>
+            <a href="#" className="nav-link px-2">
+              Promotion
+            </a>
+          </li>
+          <li>
+            <a href="#" className="nav-link px-2">
+              Contact
+            </a>
+          </li>
+          <li>
+            <a href="#" className="nav-link px-2">
+              <img
+                className="img-fluid"
+                src={cart}
+                alt="Cart"
+                style={{ width: "25px", height: "25px" }}
+              />{" "}
+            </a>
+          </li>
+        </ul>
+
+        <div className="col-md-3 text-end">
+          <button type="button" className="btn btn-outline-primary me-2">
+            Login
+          </button>
+          <button type="button" className="btn btn-primary">
+            Register
+          </button>
+        </div>
+      </header>
+    </div>
+  );
+};
+
+export { Header };
