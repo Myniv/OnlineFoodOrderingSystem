8de95e956bc211fc74ff88b85dab7c8e80c12b35
Mulyana N <mulyanan.ur26@gmail.com>
2024-10-30 17:50:38 +0700

    Update CustomerTable  add search Filter, edit, and delete data table

--- src/component/customer/CustomerTable.jsx
+++ src/component/customer/CustomerTable.jsx
@@ -1,11 +1,65 @@
 /* eslint-disable react/prop-types */
+
+import { useEffect, useState } from "react";
+
 // import { customerList } from "./CustomerList";
-const CustomerTable = ({customerList}) => {
+const CustomerTable = ({
+  customer,
+  setCustomer,
+  setIsEditingCustomer,
+  setSelectedCustomer,
+}) => {
+  const [search, setSearch] = useState("");
+  const [filteredCustomers, setFilteredCustomer] = useState(customer);
+
+  const handleSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  useEffect(() => {
+    setFilteredCustomer(
+      customer.filter((customer) => {
+        return (
+          search ===""||
+          customer.name.toLowerCase().includes(search.toLowerCase()) ||
+          customer.address.toLowerCase().includes(search.toLowerCase()) ||
+          customer.email.toLowerCase().includes(search.toLowerCase()) ||
+          customer.phonenumber.toString().includes(search)
+        );
+      })
+    );
+  }, [customer, search]);
+
+  const onDeleteCustomer = (id) => {
+    setCustomer(customer.filter((customer) => customer.id !== id));
+  };
+
+  const onEditingCustomer = (id) => {
+    const selectCustomer = customer.find((customer) => customer.id === id);
+    setSelectedCustomer(selectCustomer);
+    setIsEditingCustomer(true);
+  };
+
   return (
     <div>
       <div className="d-flex justify-content-between align-items-center">
         <h2>Customer Table</h2>
       </div>
+
+      <div className="d-flex justify-content-between align-items-center form-group">
+        <label htmlFor="search" className="form-label mb-3">
+          Search
+        </label>
+        &ensp;
+        <input
+          id="search"
+          type="text"
+          onChange={handleSearch}
+          className="form-control form-control-sm mb-3"
+          placeholder="Search Menu"
+        />
+      </div>
+
       <table className="table table-striped table-bordered">
         <thead className="thead-dark">
           <tr>
@@ -16,12 +70,32 @@ const CustomerTable = ({customerList}) => {
           </tr>
         </thead>
         <tbody>
-          {customerList.map((customerList) => (
+          {filteredCustomers.map((customerList) => (
             <tr scope="row" key={customerList.id}>
               <td key={customerList.name}>{customerList.name}</td>
               <td key={customerList.email}>{customerList.email}</td>
               <td key={customerList.phonenumber}>{customerList.phonenumber}</td>
               <td key={customerList.address}>{customerList.address}</td>
+              <td>
+                <div className="d-grid gap-2 d-md-flex justify-content-md">
+                  <button
+                    type="button"
+                    className="btn btn-primary btn-sm"
+                    onClick={() => onEditingCustomer(customerList.id)}
+                    value={"edit"}
+                  >
+                    Edit
+                  </button>
+                  <button
+                    type="button"
+                    className="btn btn-danger btn-sm"
+                    onClick={() => onDeleteCustomer(customerList.id)}
+                    value={"delete"}
+                  >
+                    Delete
+                  </button>
+                </div>
+              </td>
             </tr>
           ))}
         </tbody>
--- src/page/CustomerPage.jsx
+++ src/page/CustomerPage.jsx
@@ -2,14 +2,25 @@ import { Header } from "../component/Header";
 import { Footer } from "../component/Footer";
 // import { CustomerTable } from "../component/customer/CustomerTable";
 import { AddCustomerForm } from "../component/customer/CustomerForm";
+import { useState } from "react";
+import { customerList } from "../component/customer/CustomerList";
+import { CustomerTable } from "../component/customer/CustomerTable";
 
 function CustomerPage() {
+  const [customer, setCustomer] = useState(customerList);
+  const [isEditingCustomer, setIsEditingCustomer] = useState(false);
+  const [selectedCustomer, setSelectedCustomer] = useState(null);
   return (
     <>
       <Header />
       <div className="container">
-        {/* <CustomerTable /> */}
-        <AddCustomerForm />
+        <CustomerTable
+          customer={customer}
+          setCustomer={setCustomer}
+          setIsEditingCustomer={setIsEditingCustomer}
+          setSelectedCustomer={setSelectedCustomer}
+        />
+        {/* <AddCustomerForm /> */}
       </div>
       <Footer />
     </>
