9886fd252c035c0fdbab912f3b04cc8d884ea71d
Mulyana N <mulyanan.ur26@gmail.com>
2024-11-02 20:55:32 +0700

    Update separate orderdetail and form

--- src/page/OrderPage.jsx
+++ src/page/OrderPage.jsx
@@ -1,11 +1,37 @@
 /* eslint-disable react/prop-types */
 
+import { useState } from "react";
 import OrderForm from "../component/order/OrderForm";
+import OrderDetail from "../component/order/OrderDetail";
 
-function OrderPage({menuList, customerList}) {
+function OrderPage({ menuList, customerList }) {
+  const [orderPlaced, setOrderPlaced] = useState(false);
+
+  const [formData, setFormData] = useState({
+    customerName: "",
+    address: "",
+    menus: [], 
+  });
+
+  const [orderDetail, setOrderDetail] = useState({
+    customerName: "",
+    address: "",
+    menus: [],
+  });
   return (
     <>
-        <OrderForm menuList={menuList} customerList={customerList}/>
+      <OrderForm
+        menuList={menuList}
+        customerList={customerList}
+        formData={formData}
+        setFormData={setFormData}
+        setOrderPlaced={setOrderPlaced}
+      />
+      <OrderDetail
+        orderDetail={orderDetail}
+        setOrderDetail={setOrderDetail}
+        orderPlaced={orderPlaced}
+      />
     </>
   );
 }
